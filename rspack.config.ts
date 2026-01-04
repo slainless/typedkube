993d233b72de9d9feeb22878c7b8f8ea42bcb0c7
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2026-01-04 13:38:43 +0800

    change directory schema

--- rspack.config.ts
+++ rspack.config.ts
@@ -3,22 +3,25 @@ import path from "node:path"
 import { defineConfig } from "@rspack/cli"
 import rspack from "@rspack/core"
 
-const entries = globSync([
-	"./client_scripts/**/*.[jt]s",
-	"./server_scripts/**/*.[jt]s",
-	"./startup_scripts/**/*.[jt]s",
-])
+function scriptDirectory(dir: string, targetDirectory: string) {
+	return globSync(path.join(dir, "**/*.[jt]s")).map((file) => {
+		const abs = path.resolve(file)
+		const name = path.join(
+			targetDirectory,
+			path.relative(dir, abs).replace(/\.[jt]s$/, ""),
+		)
+		return [name, abs] as const
+	})
+}
 
 export default defineConfig({
 	context: path.resolve("."),
 	target: "es5",
-	entry: Object.fromEntries(
-		entries.map((file) => {
-			const abs = path.resolve(file)
-			const name = path.relative(".", abs).replace(/\.[jt]s$/, "")
-			return [name, abs]
-		}),
-	),
+	entry: Object.fromEntries([
+		...scriptDirectory("./scripts/client", "client_scripts"),
+		...scriptDirectory("./scripts/server", "server_scripts"),
+		...scriptDirectory("./scripts/startup", "startup_scripts"),
+	]),
 	resolve: {
 		preferRelative: true,
 	},
