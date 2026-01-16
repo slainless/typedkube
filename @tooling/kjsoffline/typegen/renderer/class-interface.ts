a95d8b9da33f9bb56e179afc4979c3b707d0e2c0
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2026-01-17 07:23:03 +0800

    fix: typescript super class rendering

--- @tooling/kjsoffline/typegen/renderer/class-interface.ts
+++ @tooling/kjsoffline/typegen/renderer/class-interface.ts
@@ -2,11 +2,11 @@ import type { WrappedRawClass } from "@tooling/kjsoffline/data"
 import { renderFields } from "./class-member"
 import { renderConstructors, renderMethods } from "./function"
 import { renderInnerClasses } from "./inner-class"
-import { renderExtends, renderImplements } from "./super-class"
+import { renderExtends } from "./super-class"
 
 export function renderClassConstructorInterface(klass: WrappedRawClass) {
 	return [
-		`interface ${klass.name({}, false, { nameSuffix: "Static" })} ${renderExtends(klass, true)} ${renderImplements(klass, true)} {`,
+		`interface ${klass.name({}, false, { nameSuffix: "Static" })} ${renderExtends(klass, true)} {`,
 		renderConstructors(klass),
 		renderMethods(klass, true),
 		renderFields(klass, true),
@@ -17,7 +17,7 @@ export function renderClassConstructorInterface(klass: WrappedRawClass) {
 
 export function renderClassInterface(klass: WrappedRawClass) {
 	return [
-		`interface ${klass.name()} ${renderExtends(klass)} ${renderImplements(klass)} {`,
+		`interface ${klass.name()} ${renderExtends(klass)} {`,
 		renderMethods(klass, false),
 		renderFields(klass, false),
 		renderInnerClasses(klass, false),
--- @tooling/kjsoffline/typegen/renderer/super-class.ts
+++ @tooling/kjsoffline/typegen/renderer/super-class.ts
@@ -2,16 +2,28 @@ import type { RawClass } from "@tooling/kjsoffline/data"
 import type { Wrapped } from "@tooling/kjsoffline/data/common"
 
 export function renderExtends(klass: Wrapped<RawClass>, asStatic = false) {
-	const superClass = klass.wrapped().superClass()
-	if (!superClass) return ""
-	const typeVariableMap = klass.typeVariableMap()
-	const wrapped = superClass.asWrapped(0, typeVariableMap)
-	return `extends ${wrapped.referenceName(typeVariableMap, { nameSuffix: asStatic ? "Static" : "" })}`
-}
-
-export function renderImplements(klass: Wrapped<RawClass>, asStatic = false) {
 	const interfaces = klass.wrapped().interfaces()
+	const superClass = klass.wrapped().superClass()
+	if (superClass) interfaces.push(superClass)
+
 	if (interfaces.length === 0) return ""
 	const typeVariableMap = klass.typeVariableMap()
-	return `implements ${interfaces.map((v) => v.asWrapped(0, typeVariableMap).referenceName(typeVariableMap, { nameSuffix: asStatic ? "Static" : "" })).join(", ")}`
+	return [
+		"extends ",
+		interfaces
+			.map((v) =>
+				v.asWrapped(0, typeVariableMap).referenceName(typeVariableMap, {
+					nameSuffix: asStatic ? "Static" : "",
+					typescriptCompatibility: true,
+				}),
+			)
+			.join(", "),
+	].join("")
 }
+
+// export function renderImplements(klass: Wrapped<RawClass>, asStatic = false) {
+// 	const interfaces = klass.wrapped().interfaces()
+// 	if (interfaces.length === 0) return ""
+// 	const typeVariableMap = klass.typeVariableMap()
+// 	return `implements ${interfaces.map((v) => v.asWrapped(0, typeVariableMap).referenceName(typeVariableMap, { nameSuffix: asStatic ? "Static" : "" })).join(", ")}`
+// }
