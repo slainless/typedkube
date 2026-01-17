df66b2ea505cb312b93a7337a93e501af847daa7
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2026-01-18 06:09:42 +0800

    add alias for java primitive types

---
+++ typings/primitives.d.ts
@@ -0,0 +1,7 @@
+declare type int = number
+declare type float = number
+declare type double = number
+declare type byte = number
+declare type short = number
+declare type long = number
+declare type char = string
