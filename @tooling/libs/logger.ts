96a23cb6855b3a9aabefd50b707f135d96f6a7ad
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2026-01-17 21:18:10 +0800

    fix: setup log directory and truncate

--- @tooling/libs/logger.ts
+++ @tooling/libs/logger.ts
@@ -1,6 +1,8 @@
 import { getFileSink } from "@logtape/file"
+import { mkdir, truncate } from "node:fs/promises"
 import { type Config, configure, getConsoleSink } from "@logtape/logtape"
 import { getPrettyFormatter } from "@logtape/pretty"
+import { dirname } from 'node:path'
 
 export async function configureLogger(outputPath?: string) {
 	const tapeConfig: Config<any, any> = {
@@ -16,6 +18,10 @@ export async function configureLogger(outputPath?: string) {
 	} as const
 
 	if (outputPath) {
+		await mkdir(dirname(outputPath), { recursive: true })
+		try {
+			await truncate(outputPath)
+		} catch (error) {}
 		tapeConfig.sinks.file = getFileSink(outputPath)
 		tapeConfig.loggers[0]?.sinks?.push("file")
 	}
