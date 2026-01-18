9c7af60630993f5a886b8874e0e9be0daf6059b3
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2026-01-18 17:43:58 +0800

    fix: broken path in config

--- typedkube.config.ts
+++ typedkube.config.ts
@@ -1,11 +1,11 @@
-import { resolve } from "node:path"
+import { join, resolve } from "node:path"
 
-export const KUBEJS_PATH = resolve(process.cwd(), "..", "kubejs")
-export const KUBEJS_OFFLINE_HTML_DUMP_PATH = resolve(
+export const KUBEJS_PATH = join(process.cwd(), "..", "kubejs")
+export const KUBEJS_OFFLINE_HTML_DUMP_PATH = join(
 	KUBEJS_PATH,
 	"documentation",
 	"index.html",
 )
 
-export const TYPEGEN_OUTPUT_PATH = resolve(process.cwd(), "typings", "typegen")
+export const TYPEGEN_OUTPUT_PATH = join(process.cwd(), "typings", "typegen")
 export const TYPEDKUBE_OUTPUT_PATH = KUBEJS_PATH
